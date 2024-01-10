89f3a5f85d01f04206577f9a81cc3c039da0da7f
jiamid <jiamid.t@binarywalk.com>
2024-01-10 17:13:38 +0800

    新增图标

--- lib/pages/game_config.dart
+++ lib/pages/game_config.dart
@@ -17,8 +17,31 @@ Map boxColorMap = {
   13: Colors.brown,
   14: Colors.teal,
   15: Colors.deepPurpleAccent,
+  16: Colors.tealAccent,
+  17: Colors.deepPurple,
+  18: Colors.blueGrey,
+
 };
-final List<int> allType = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+final List<int> allType = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  12,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18
+];
 
 final Map<int, Map<int, List<int>>> levelOffsetMap = {
   1: {
@@ -58,10 +81,10 @@ final Map<int, Map<int, List<int>>> levelOffsetMap = {
 Map<int, List<int>> levelTypeMap = {
   1: [6, 6, 6],
   2: [6, 6, 3, 3, 3, 3, 3, 3],
-  3: [6, 6, 6, 6, 6, 6, 6, 6, 3, 3, 3, 3, 3, 3],
+  3: [6, 6, 6, 3, 6, 6, 6, 6, 3, 3, 3, 3, 3, 3, 3],
   4: [6, 6, 12, 3, 6, 9, 9, 6, 6, 9, 3, 3, 6, 18, 9],
   5: [6, 6, 12, 6, 6, 9, 9, 6, 6, 9, 6, 6, 18, 6],
-  6: [18, 18, 15, 15, 15, 12, 12, 12, 12, 12, 9, 6, 6, 3],
+  6: [18, 18, 15, 15, 15, 12, 12, 9, 6, 6, 6, 6, 6, 9, 6, 3, 3],
 };
 
 Map<int, List<List<List<int>>>> levelMap = {
