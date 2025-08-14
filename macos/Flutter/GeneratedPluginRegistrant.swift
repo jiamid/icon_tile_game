907474add236a8a34c89996110cdbcebbc54506d
Jiamid <JiamidTan@poweronlabs.ai>
2025-08-14 16:48:03 +0800

    fix:update jdk version

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import in_app_purchase_storekit
 import package_info_plus
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
